94f9c8224b8ac72aa80fb390154b9ae424e99155
Matthew Lin <Matthew Lin>
2016-10-09 17:57:28 -0400

    Further development on node code

--- liri.js
+++ liri.js
@@ -1,45 +1,38 @@
-var keys = require('./keys.js');
-keys.more = {'extra' : 'more'}
+var client = require('./keys.js');
+client.more = {'extra' : 'more'}
 
-// var client = process.argv[2];
-
-keys.client
-
-var stream = client.stream('statuses/filter', {track: 'javascript'});
-stream.on('data', function(event) {
-  console.log(event && event.text);
-});
- 
-stream.on('error', function(error) {
-  throw error;
+client.get('search/tweets', {q: 'keys.js'}, function(error, tweets, response) {
+   console.log(tweets);
 });
 
 console.log("Twitter")
-console.log(keys.twitterKeys);
+console.log(client.client);
+
+var client = process.argv[2];
+
+var client = keys.client;
 
 // ====================================
 
-// var spotify = require('spotify');
+var spotify = require('spotify');
 
-// spotify.get({ type: 'track', query: process.argv[2]}, function(err, data) {
-//     if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
- 
-//     // Do something with 'data' 
-// });
+spotify.search({ type: 'track', query: process.argv[2]}, function(err, data) {
+    if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+});
 
 
 
 // // ==================================
 
-// var request = require('request');
-// var movie = process.argv[3];
-// request('http://www.omdbapi.com/?' + movie + "t=&y=&plot=short&r=json&tomatoes=", function (error, response, body) {
+var request = require('request');
+var movie = process.argv[3];
+request('http://www.omdbapi.com/?' + movie + "t=&y=&plot=short&r=json&tomatoes=", function (error, response, body) {
 
-// 	if (!error && response.statusCode == 200) {
+	if (!error && response.statusCode == 200) {
 
-// 		console.log("The movie's information is: " + JSON.parse(body)["imdbInfo"])
-// 	}
-// });
+		console.log("The movie's information is: " + JSON.parse(body)["imdbInfo"])
+	}
+});
