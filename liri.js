448867968ed924d2bbbb4e12ddae678118708278
Matthew Lin <Matthew Lin>
2016-10-09 17:32:41 -0400

    Added preliminary code for twitter, spotify, and ombd

--- liri.js
+++ liri.js
@@ -1,5 +1,45 @@
 var keys = require('./keys.js');
 keys.more = {'extra' : 'more'}
 
+// var client = process.argv[2];
+
+keys.client
+
+var stream = client.stream('statuses/filter', {track: 'javascript'});
+stream.on('data', function(event) {
+  console.log(event && event.text);
+});
+ 
+stream.on('error', function(error) {
+  throw error;
+});
+
 console.log("Twitter")
 console.log(keys.twitterKeys);
+
+// ====================================
+
+// var spotify = require('spotify');
+
+// spotify.get({ type: 'track', query: process.argv[2]}, function(err, data) {
+//     if ( err ) {
+//         console.log('Error occurred: ' + err);
+//         return;
+//     }
+ 
+//     // Do something with 'data' 
+// });
+
+
+
+// // ==================================
+
+// var request = require('request');
+// var movie = process.argv[3];
+// request('http://www.omdbapi.com/?' + movie + "t=&y=&plot=short&r=json&tomatoes=", function (error, response, body) {
+
+// 	if (!error && response.statusCode == 200) {
+
+// 		console.log("The movie's information is: " + JSON.parse(body)["imdbInfo"])
+// 	}
+// });
