2be2ec555977aa84f988b9dfdffb7bf8ab137e84
Matthew Lin <Matthew Lin>
2016-10-12 10:06:39 -0400

    Improved spotify and omdb sections to fully functioning state

--- liri.js
+++ liri.js
@@ -1,3 +1,6 @@
+var request = require('request');
+var movie = process.argv[3];
+
 var keys = require('./keys.js');
 keys.more = {'extra' : 'more'}
 
@@ -44,8 +47,8 @@ switch(action){
         spotify();
     break;
 
-    case 'ombd':
-        omdb();
+    case 'omdb':
+        omdb(value);
     break;
 }
 
@@ -68,34 +71,56 @@ function twitter(){
 // =======================
 function spotify(){
 var spotify = require('spotify');
-var spotifySong = process.argv[3];
+// var spotifySong = process.argv[3];
 
-spotify.search({ type: 'track', query: spotifySong}, function(err, data){
+spotify.search({ type: 'track', query: process.argv[3]}, function(err, data){
     if (err) {
         console.log('Error occurred: ' + err);
         return;
         }
     else {
-        console.log(spotify.search);
+        // console.log(spotify.search);
+            // console.log(data);
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song Title: " + data.tracks.items[0].name);
+            console.log("Album Title: " + data.tracks.items[0].album.name);
+            console.log("Spotify URL: " + data.tracks.items[0].preview_url);
+
     }
+    // spotifySong = "Title" + JSON.parse(data).Name + "\n" + 
+    //                 "Artist" + JSON.parse(data).artists.name + "\n" +
+    //                 "Preview" + JSON.parse(data).preview_url + "\n" +
+    //             "Album" + JSON.parse(data).Albums.Title.Name + "\n" + 
+    // console.log(JSON.stringify(data, null, 2));
     });
 }
 //========================
 
-function omdb(){
-var request = require('request');
-var movie = process.argv[3];
+function omdb(value){
 
-var queryUrl = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=short&r=json&tomatoes=';
-  
+// console.log("Does anything work...", value);
+var queryUrl = 'http://www.omdbapi.com/?t=' + value +'&y=&plot=short&r=json&tomatoes=true';
+  // console.log(queryUrl);
 request(queryUrl, function (error, response, body) {
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode == 200) {
         // Parse the body of the site and recover just the imdbRating
         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
-        console.log("Release Year: " + JSON.parse(body)["Year"])
+        console.log("Title: " + JSON.parse(body)["Title"]);
+        console.log("Rating: " + JSON.parse(body)["Rated"]);
+        console.log("Release Year: " + JSON.parse(body)["Year"]);
+        console.log("Country: " + JSON.parse(body)["Country"]);
+        console.log("Language: " + JSON.parse(body)["Language"]);
+        console.log("Plot: " + JSON.parse(body)["Plot"]);
+        console.log("Actors: " + JSON.parse(body)["Actors"]);
+        console.log("RT Rating: " + JSON.parse(body)["tomatoMeter"]);
+        console.log("RT Site: " + JSON.parse(body)["tomatoURL"]);
+        // console.log(JSON.parse(body));
+    }else{
+        console.log(error);
     }
 });
+
 
 }
 
